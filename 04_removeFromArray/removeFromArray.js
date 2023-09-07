3bbb16215fa7df31cf945627958027a1a60c360a
JPrime <MLGJPrime@users.noreply.github.com>
2023-09-07 16:55:50 +0200

    Finished exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const newArray = []
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray
 };
+
+console.log(removeFromArray([1, 3, 2, 3, 4, 3], 3, 7, "spam", 1))
 
 // Do not edit below this line
 module.exports = removeFromArray;
