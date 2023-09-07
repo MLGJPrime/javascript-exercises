943bd31d6bdebad5ad8671ec69d9fe0ebb755999
JPrime <MLGJPrime@users.noreply.github.com>
2023-09-07 17:17:09 +0200

    Finished exercise 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,14 @@
 const removeFromArray = function(array, ...args) {
-    const newArray = []
+    const newArray = [];
     array.forEach((item) => {
         if (!args.includes(item)) {
             newArray.push(item);
         }
     })
-    return newArray
+    return newArray;
 };
 
-console.log(removeFromArray([1, 3, 2, 3, 4, 3], 3, 7, "spam", 1))
+console.log(removeFromArray([1, 3, 2, 3, 4, 3], 3, 7, "spam", 1));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) [min, max] = [max, min];
+    let sum = 0;
+    for (i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+console.log(sumAll("30", 1))
 
 // Do not edit below this line
 module.exports = sumAll;
