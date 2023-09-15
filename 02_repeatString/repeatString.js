68f3fcb04bf78964512fff716ecd273d75ace951
JPrime <132490938+MLGJPrime@users.noreply.github.com>
2023-09-15 15:19:42 +0200

    Finished exercises 01, 02, 03, 04, 05, 06

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+function repeatString(word, num) {
+    if (num < 0) return "ERROR";
+    let string = "";
+    for (i = 1;i <= num;i++) {
+        string += word;
+    }
+    return string;
+}
 
-};
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
