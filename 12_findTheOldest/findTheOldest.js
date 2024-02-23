0b77e5edf607ec8821dc9391f53a72c345a1837a
JPrime <132490938+MLGJPrime@users.noreply.github.com>
2024-02-23 16:45:48 +0100

    Finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const getAge = (person) => {
+    let yearOfDeath = person.yearOfDeath || new Date().getFullYear();
+    return yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        let oldestAge = getAge(oldest);
+        let currentAge = getAge(currentPerson);
+        return (currentAge > oldestAge) ? currentPerson : oldest;
+    });
 };
 
 // Do not edit below this line
