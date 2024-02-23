7ceaeeb71d28ed58126e3b015c6814ef4c1844f1
JPrime <132490938+MLGJPrime@users.noreply.github.com>
2024-02-23 16:20:56 +0100

    Finished exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let phrase = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let phraseReverse = phrase.split("").reverse().join("");
+    return phrase === phraseReverse;
 };
+
+// console.log(palindromes('racecar'));
+// console.log(palindromes('tacos'));
 
 // Do not edit below this line
 module.exports = palindromes;
